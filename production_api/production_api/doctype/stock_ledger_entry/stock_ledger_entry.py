5e10a94507fbe43f3940eed63a0d8f41fdaf5966
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-07-07 13:13:12 +0530

    stock ledger entry

---
+++ production_api/production_api/doctype/stock_ledger_entry/stock_ledger_entry.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2021, Essdee and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from frappe.model.document import Document
+
+class StockLedgerEntry(Document):
+	pass
---
+++ production_api/production_api/doctype/stock_ledger_entry/test_stock_ledger_entry.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Essdee and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestStockLedgerEntry(unittest.TestCase):
+	pass
