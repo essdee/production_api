efb9f56fd8b281100125db34ab05cb4b3bb6da27
Kaviya Periyasamy <kaviya.3111999@gmail.com>
2021-07-07 14:05:43 +0530

    update stock settings

---
+++ production_api/production_api/doctype/stock_settings/stock_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Essdee and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Stock Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
