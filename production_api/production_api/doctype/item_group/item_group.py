f72f6d1d7c6e968299588e3928b8e65d17bfe20a
dwarakavikraman <dwarakavikraman@gmail.com>
2023-05-17 15:49:24 +0530

    Added Item Group

---
+++ production_api/production_api/doctype/item_group/item_group.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Essdee and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+class ItemGroup(NestedSet):
+	pass
