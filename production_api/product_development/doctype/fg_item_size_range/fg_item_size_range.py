bfc2269f4f2b12147d68b4559fd5d070787eb3a3
Dwaraka Vikraman <dwarakavikraman@gmail.com>
2023-12-22 16:16:02 +0530

    FG Item Category and Size Range

---
+++ production_api/product_development/doctype/fg_item_size_range/fg_item_size_range.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2023, Essdee and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from frappe.model.document import Document
+
+class FGItemSizeRange(Document):
+	def validate(self):
+		if not self.uid:
+			frappe.throw(_("UID is required."))
+		# Check if the uid is unique if new.
+		if self.is_new():
+			if frappe.db.exists({"doctype": "FG Item Size Range", "uid": self.uid}):
+				frappe.throw(_("UID {0} already exists.").format(self.uid))
