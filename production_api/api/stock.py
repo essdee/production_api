afd2dac87a544a232c9cfc0136d20f8a88c22686
SAKTHI KUMAR P <skthi123msd@gmail.com>
2024-09-21 16:46:44 +0530

    bin and Stock Reservation with SMS

--- production_api/api/stock.py
+++ production_api/api/stock.py
@@ -1,6 +1,7 @@
 import frappe, json
 from frappe.utils import today, add_to_date
 from production_api.mrp_stock.report.stock_balance.stock_balance import execute as stock_balance
+from production_api.mrp_stock.doctype.bin.bin import get_stock_balance_bin
 from six import string_types
 
 @frappe.whitelist()
@@ -21,7 +22,13 @@ def get_stock(item, warehouse, remove_zero_balance_item=1):
         'lot': fg_lot,
         'remove_zero_balance_item': remove_zero_balance_item
     }
-    _, data = stock_balance(filters)
+    data  = get_stock_balance_bin(
+        warehouse,
+        fg_lot,item,
+        remove_zero_balance_item
+    )
+    # _,data = stock_balance(filters)
+    
     item_wh_map = {}
     for d in data:
         group_by_key = get_group_by_key(d)
@@ -30,7 +37,7 @@ def get_stock(item, warehouse, remove_zero_balance_item=1):
                 {
                     "item": d['item'],
                     "bal_qty": 0.0,
-                    "uom": d['stock_uom'],
+                    "uom": d['uom'],
                 }
             )
         item_wh_map[group_by_key]['bal_qty'] += d['bal_qty']
@@ -38,8 +45,6 @@ def get_stock(item, warehouse, remove_zero_balance_item=1):
     return item_wh_map
 
 def get_group_by_key(row) -> str:
-    # group_by_key = [row['item'], row['warehouse']]
-    # return tuple(group_by_key)
     return row['item']
 
 @frappe.whitelist()
@@ -59,6 +64,14 @@ def make_dispatch_stock_entry(items, warehouse, packing_slip):
     })
     index = 0
     for item in items:
+        
+        sre = frappe.get_doc("Stock Reservation Entry", item['sre'])
+        sre.delivered_qty += item['qty']
+        
+        sre.db_update()
+        sre.update_status()
+        sre.update_reserved_stock_in_bin()
+        
         ste.append("items", {
             'item': item['item'],
             'qty': item['qty'],
@@ -68,6 +81,8 @@ def make_dispatch_stock_entry(items, warehouse, packing_slip):
             'row_index': index,
         })
         index += 1
+        
+        
     ste.flags.allow_from_sms = True
     ste.save()
     ste.submit()
@@ -85,3 +100,45 @@ def get_default_fg_lot(raise_error=True):
     if not (fg_lot := stock_settings.default_fg_lot) and raise_error:
         frappe.throw("Please set default FG Lot in settings")
     return fg_lot
+
+@frappe.whitelist()
+def create_stock_reservation_entries(
+	packing_slip,
+	items_details: list[dict] | None = None,
+) -> None:
+	"""Creates Stock Reservation Entries for Sales Order Items."""
+	from production_api.mrp_stock.doctype.stock_reservation_entry.stock_reservation_entry import (
+		create_stock_reservation_entries_for_so_items as create_stock_reservation_entries,
+	)
+	return create_stock_reservation_entries(
+		"Packing Slip",
+        voucher_no=packing_slip,
+		items_details=items_details,
+	)
+ 
+@frappe.whitelist()
+def cancel_stock_reservation_entries(packing_slip, sre_list=None, notify=True) -> None:
+	"""Cancel Stock Reservation Entries for Sales Order Items."""
+	from production_api.mrp_stock.doctype.stock_reservation_entry.stock_reservation_entry import (
+		cancel_stock_reservation_entries,
+	)
+	cancel_stock_reservation_entries(
+		voucher_type="Packing Slip", voucher_no=packing_slip, sre_list=sre_list, notify=notify
+	)
+ 
+@frappe.whitelist()
+def update_stock_reservation_entries(packing_slip, item_details):
+    
+    from production_api.mrp_stock.doctype.stock_reservation_entry.stock_reservation_entry import (
+        update_stock_reservation_entries,
+    )
+    
+    if isinstance(item_details,string_types):
+        item_details = json.loads(item_details)
+    
+        
+    return update_stock_reservation_entries(
+        voucher_type = "Packing Slip",
+        voucher_no = packing_slip,
+        item_details = item_details
+    )
---
+++ production_api/mrp_stock/doctype/stock_reservation_entry/stock_reservation_entry.py
@@ -0,0 +1,366 @@
+# Copyright (c) 2024, Essdee and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+from frappe.query_builder.functions import Sum
+from frappe.utils import cint, flt, nowdate, nowtime
+from production_api.api.stock import get_default_fg_lot
+from frappe import _
+from production_api.mrp_stock.utils import get_stock_balance
+
+
+class StockReservationEntry(Document):
+	def validate(self) -> None:
+		self.validate_mandatory()
+		self.validate_uom_is_integer()
+  
+	def on_submit(self) -> None:
+		self.update_status()
+		self.update_reserved_stock_in_bin()
+	
+	def on_update_after_submit(self) -> None:
+		self.can_be_updated()
+		self.validate_uom_is_integer()
+		self.update_status()
+		self.update_reserved_stock_in_bin()
+		self.reload()
+
+	def on_cancel(self) -> None:
+		self.update_status()
+		self.update_reserved_stock_in_bin()
+ 
+	def validate_mandatory(self) -> None:
+		"""Raises an exception if mandatory fields are not set."""
+
+		mandatory = [
+			"item_code",
+			"warehouse",
+			"voucher_type",
+			"voucher_no",
+			"lot",
+			"available_qty",
+			"stock_uom",
+			"reserved_qty",
+		]
+		for d in mandatory:
+			if not self.get(d):
+				msg = _("{0} is required").format(self.meta.get_label(d))
+				frappe.throw(msg)
+    
+	def validate_uom_is_integer(self) -> None:
+		"""Validates `Reserved Qty` with Stock UOM."""
+
+		if cint(frappe.db.get_value("UOM", self.stock_uom, "must_be_whole_number", cache=True)):
+			if cint(self.reserved_qty) != flt(self.reserved_qty, self.precision("reserved_qty")):
+				msg = _(
+					"Reserved Qty ({0}) cannot be a fraction. To allow this, disable '{1}' in UOM {3}."
+				).format(
+					flt(self.reserved_qty, self.precision("reserved_qty")),
+					frappe.bold(_("Must be Whole Number")),
+					frappe.bold(self.stock_uom),
+				)
+				frappe.throw(msg)
+	
+	def update_status(self, status: str | None = None, update_modified: bool = True) -> None:
+		"""Updates status based on Voucher Qty, Reserved Qty and Delivered Qty."""
+
+		if not status:
+			if self.docstatus == 2:
+				status = "Cancelled"
+			elif self.docstatus == 1:
+				if self.reserved_qty == self.delivered_qty:
+					status = "Delivered"
+				elif self.delivered_qty and self.delivered_qty < self.reserved_qty:
+					status = "Partially Delivered"
+				elif self.reserved_qty == self.voucher_qty:
+					status = "Reserved"
+				else:
+					status = "Partially Reserved"
+			else:
+				status = "Draft"
+
+		frappe.db.set_value(self.doctype, self.name, "status", status, update_modified=update_modified)
+  
+	def update_reserved_stock_in_bin(self) -> None:
+		"""Updates `Reserved Stock` in Bin."""
+		from production_api.utils import get_or_make_bin
+		bin_name = get_or_make_bin(self.item_code, self.warehouse, self.lot)
+		bin_doc = frappe.get_cached_doc("Bin", bin_name)
+		bin_doc.update_reserved_stock()
+	
+	def can_be_updated(self) -> None:
+		"""Raises an exception if `Stock Reservation Entry` is not allowed to be updated."""
+
+		if self.status in ("Partially Delivered", "Delivered"):
+			msg = _(
+				"{0} {1} cannot be updated. If you need to make changes, we recommend canceling the existing entry and creating a new one."
+			).format(self.status, self.doctype)
+			frappe.throw(msg)
+
+		if self.from_voucher_type == "Pick List":
+			msg = _(
+				"Stock Reservation Entry created against a Pick List cannot be updated. If you need to make changes, we recommend canceling the existing entry and creating a new one."
+			)
+			frappe.throw(msg)
+
+		if self.delivered_qty > 0:
+			msg = _("Stock Reservation Entry cannot be updated as it has been delivered.")
+			frappe.throw(msg)
+
+def create_stock_reservation_entries_for_so_items(
+    voucher_type,
+	voucher_no,
+	items_details,
+) -> None:
+	"""Creates Stock Reservation Entries for Sales Order Items."""
+
+	from production_api.utils import get_unreserved_qty
+ 
+	warning_msg = ""
+
+	if len(items_details) == 0:
+		return {
+			"status" : 422,
+			"error" : True,
+			"message" : "Stock Allocation Requires Items",
+			"warning" : warning_msg
+		}
+	items = []
+	if items_details:
+		for item in items_details:
+			
+			ps_item = {}
+			ps_item['item_name'] = item.get("item_name")
+			ps_item['warehouse'] = item.get("warehouse")
+			ps_item['voucher_detail_no'] = item.get("uuid")
+			ps_item['lot'] = get_default_fg_lot()
+			ps_item['uom'] = item.get("uom")
+			ps_item['qty_to_reserve'] = (
+				flt(item.get("qty"))
+			)
+
+			items.append(ps_item)
+	reserved_qty_details = get_sre_reserved_qty_details_for_voucher(voucher_type, voucher_no)
+	
+	savepoint = f"srecreate"
+	frappe.db.savepoint(savepoint)
+ 
+	response_dict = {}
+ 
+	for item in items :
+
+		unreserved_qty = get_unreserved_qty(item, reserved_qty_details)
+
+		# Stock is already reserved for the item, notify the user and skip the item.
+		if unreserved_qty <= 0:
+			frappe.db.rollback()
+			return {
+				"status" : 422,
+				"error" : True,
+				"message" : f"Stock is Already Reserved For {item['item_name']}",
+				"warning" : warning_msg
+			}
+
+		available_qty_to_reserve = get_available_qty_to_reserve(item['item_name'], item['warehouse'], item['lot'])
+
+		# No stock available to reserve, notify the user and skip the item.
+		if available_qty_to_reserve <= 0:
+			frappe.db.rollback()
+			return {
+				"status" : 422,
+				"error" : True,
+				"message" : f"Stock Not Available For {item['item_name']}",
+				"warning" : warning_msg
+			}
+
+		# The quantity which can be reserved.
+		qty_to_be_reserved = min(unreserved_qty, available_qty_to_reserve)
+
+		if 'qty_to_reserve' in item:
+			if item['qty_to_reserve'] <= 0:
+				warning_msg += f"""<br> Quantity to reserve for the Item {item['item_name']} should be greater than 0."""
+				continue
+
+		if item['qty_to_reserve'] > qty_to_be_reserved:
+			#rollback to before creation of sre for this packing slip
+			frappe.db.rollback()
+			
+			return {
+				"status" : 422,
+				"error" : True,
+				"message" : f"Stock Not Available For {item['item_name']}",
+				"warning" : warning_msg
+			}
+		else:
+			qty_to_be_reserved = min(qty_to_be_reserved, item['qty_to_reserve'])
+
+  
+		sre = frappe.new_doc("Stock Reservation Entry")
+
+		sre.item_code = item['item_name']
+		sre.warehouse = item['warehouse']
+		sre.lot = item['lot']
+
+		sre.voucher_type = voucher_type
+		sre.voucher_no = voucher_no
+		sre.voucher_detail_no = item['voucher_detail_no']
+		sre.available_qty = available_qty_to_reserve
+		sre.reserved_qty = qty_to_be_reserved
+		sre.voucher_qty = item['qty_to_reserve']
+		sre.stock_uom = item['uom']
+
+		sre.save()
+		sre.submit()
+  
+		response_dict[item['voucher_detail_no']] = sre.name
+  
+	return response_dict
+	
+
+def get_sre_reserved_qty_details_for_voucher(voucher_type: str, voucher_no: str) -> dict:
+	"""Returns a dict like {"voucher_detail_no": "reserved_qty", ... }."""
+
+	sre = frappe.qb.DocType("Stock Reservation Entry")
+	data = (
+		frappe.qb.from_(sre)
+		.select(
+			sre.voucher_detail_no,
+			(Sum(sre.reserved_qty) - Sum(sre.delivered_qty)).as_("reserved_qty"),
+		)
+		.where(
+			(sre.docstatus == 1)
+			& (sre.voucher_type == voucher_type)
+			& (sre.voucher_no == voucher_no)
+			& (sre.status.notin(["Delivered", "Cancelled"]))
+		)
+		.groupby(sre.voucher_detail_no)
+	).run(as_list=True)
+
+	data =  frappe._dict(data)
+	return data
+
+def get_available_qty_to_reserve(
+	item_code: str, warehouse: str,lot :str, ignore_sre=None
+) -> float:
+	"""Returns `Available Qty to Reserve (Actual Qty - Reserved Qty)` for Item, Warehouse and Batch combination."""
+ 
+	available_qty = get_stock_balance(item_code,warehouse=warehouse,lot=lot)
+
+	if available_qty:
+		sre = frappe.qb.DocType("Stock Reservation Entry")
+		query = (
+			frappe.qb.from_(sre)
+			.select(Sum(sre.reserved_qty - sre.delivered_qty))
+			.where(
+				(sre.docstatus == 1)
+				& (sre.item_code == item_code)
+				& (sre.warehouse == warehouse)
+				& (sre.reserved_qty >= sre.delivered_qty)
+				& (sre.status.notin(["Delivered", "Cancelled"]))
+			)
+		)
+
+		if ignore_sre:
+			query = query.where(sre.name != ignore_sre)
+
+		reserved_qty = query.run()[0][0] or 0.0
+
+		if reserved_qty:
+			return available_qty - reserved_qty
+
+	return available_qty
+
+def get_sre_reserved_qty_for_item_and_warehouse(item_code: str, warehouse: str | None = None, lot: str | None = None) -> float:
+	"""Returns current `Reserved Qty` for Item and Warehouse combination."""
+
+	sre = frappe.qb.DocType("Stock Reservation Entry")
+	query = (
+		frappe.qb.from_(sre)
+		.select(Sum(sre.reserved_qty - sre.delivered_qty).as_("reserved_qty"))
+		.where(
+			(sre.docstatus == 1)
+			& (sre.item_code == item_code)
+			& (sre.status.notin(["Delivered", "Cancelled"]))
+		)
+		.groupby(sre.item_code, sre.warehouse, sre.lot)
+	)
+
+	if warehouse:
+		query = query.where(sre.warehouse == warehouse)
+	if lot:
+		query = query.where(sre.lot == lot)
+
+	reserved_qty = query.run(as_list=True)
+	return flt(reserved_qty[0][0]) if reserved_qty else 0.0
+
+def cancel_stock_reservation_entries(
+	voucher_type: str | None = None,
+	voucher_no: str | None = None,
+	sre_list: list | None = None,
+	notify: bool = True,
+) -> None:
+	"""Cancel Stock Reservation Entries."""
+
+	if not sre_list:
+		sre_list = {}
+
+		if voucher_type and voucher_no:
+			sre_list = get_stock_reservation_entries_for_voucher(
+				voucher_type, voucher_no, fields=["name"]
+			)
+
+
+		sre_list = [d.name for d in sre_list]
+
+	if sre_list:
+		for sre in sre_list:
+			frappe.get_doc("Stock Reservation Entry", sre).cancel()
+
+		if notify:
+			msg = _("Stock Reservation Entries Cancelled")
+			frappe.msgprint(msg, alert=True, indicator="red")
+
+@frappe.whitelist()
+def get_stock_reservation_entries_for_voucher(
+	voucher_type: str,
+	voucher_no: str,
+	voucher_detail_no: str | None = None,
+	fields: list[str] | None = None,
+	ignore_status: bool = False,
+) -> list[dict]:
+	"""Returns list of Stock Reservation Entries against a Voucher."""
+
+	if not fields or not isinstance(fields, list):
+		fields = [
+			"name",
+			"item_code",
+			"warehouse",
+			"lot",
+			"reserved_qty",
+			"delivered_qty",
+		]
+
+	sre = frappe.qb.DocType("Stock Reservation Entry")
+	query = (
+		frappe.qb.from_(sre)
+		.where((sre.docstatus == 1) & (sre.voucher_type == voucher_type) & (sre.voucher_no == voucher_no))
+		.orderby(sre.creation)
+	)
+
+	for field in fields:
+		query = query.select(sre[field])
+
+	if voucher_detail_no:
+		query = query.where(sre.voucher_detail_no == voucher_detail_no)
+
+	if ignore_status:
+		query = query.where(sre.status.notin(["Delivered", "Cancelled"]))
+
+	return query.run(as_dict=True)
+
+def update_stock_reservation_entries(voucher_type, voucher_no, item_details):
+    
+    cancel_stock_reservation_entries(voucher_type,voucher_no)
+    
+    return create_stock_reservation_entries_for_so_items(voucher_type, voucher_no,item_details)
