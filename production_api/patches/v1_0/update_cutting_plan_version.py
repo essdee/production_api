03549f422baf363889a1d70253087a791970ea32
MohammedAnassBE <mohammedanasman123@gmail.com>
2025-02-28 19:01:48 +0530

    patch fix

--- production_api/patches/v1_0/update_cutting_plan_version.py
+++ production_api/patches/v1_0/update_cutting_plan_version.py
@@ -3,6 +3,11 @@ import frappe
 def execute():
     frappe.db.sql(
         """
-            Update `tabCutting Plan` set version = 'V1' where 1 = 1
+            Update `tabCutting Plan` as t1 join `tabCutting LaySheet` as t2 ON t1.name = t2.cutting_plan Set t1.version = 'V1'  
+        """
+    )
+    frappe.db.sql(
+        """
+            Update `tabCutting Plan` set version = 'V2' where version is null;
         """
     )
