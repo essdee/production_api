3090709eaddb3ebab8a7a18638bd85ab1897c921
MohammedAnassBE <mohammedanasman123@gmail.com>
2025-04-08 10:24:51 +0530

    minor fix

--- production_api/production_api/doctype/goods_received_note/goods_received_note.js
+++ production_api/production_api/doctype/goods_received_note/goods_received_note.js
@@ -202,61 +202,6 @@ frappe.ui.form.on('Goods Received Note', {
 				frm._cancel()
 			})
 		}
-		// if(frm.doc.docstatus == 1 && frm.doc.against == "Work Order" && frm.doc.rework_created == 0){
-		// 	frm.add_custom_button("Create Rework",()=> {
-		// 		let d =new frappe.ui.Dialog({
-		// 			title : "Select the type of rework",
-		// 			fields: [
-		// 				{
-		// 					"fieldname":"supplier_type",
-		// 					"fieldtype":"Select",
-		// 					"label":"Supplier Type",
-		// 					"options":"Same Supplier\nDifferent Supplier",
-		// 					"reqd":true,
-		// 				},
-		// 				{
-		// 					"fieldname":"rework_type",
-		// 					"fieldtype":"Select",
-		// 					"label":"Rework Type",
-		// 					"options":"No Cost\nNet Cost Nil\nAdditional Cost",
-		// 					"reqd":true,
-		// 				},
-		// 			],
-		// 			primary_action(values){
-		// 				if(values.supplier_type == 'Different Supplier'){
-		// 					d.hide()
-		// 					let dialog = new frappe.ui.Dialog({
-		// 						title:"Select Supplier",
-		// 						fields: [
-		// 							{
-		// 								"fieldname":"supplier",
-		// 								"fieldtype":"Link",
-		// 								"options":"Supplier",
-		// 								"label":"Supplier",
-		// 							},
-		// 							{
-		// 								"fieldname":"supplier_address",
-		// 								"fieldtype":"Link",
-		// 								"options":"Address",
-		// 								"label":"Supplier Address",
-		// 							}
-		// 						],
-		// 						primary_action(val){
-		// 							dialog.hide()
-		// 							make_rework(frm, val.supplier, val.supplier_address, frm.doc.delivery_address, values.rework_type, values.supplier_type)
-		// 						}
-		// 					})
-		// 					dialog.show()
-		// 				}
-		// 				else{
-		// 					d.hide()
-		// 					make_rework(frm, frm.doc.supplier, frm.doc.supplier_address, frm.doc.delivery_address, values.rework_type, values.supplier_type)
-		// 				}
-		// 			}
-		// 		})
-		// 		d.show()
-		// 	})
-		// }
 	},
 	save_item_details: function(frm) {
 		if(frm.itemEditor && !frm.doc.is_return){
@@ -448,28 +393,6 @@ frappe.ui.form.on('Goods Received Note', {
 		}
 	},
 });
-
-function make_rework(frm, supplier, supplier_address, delivery_address, rework_type, supplier_type){
-	frappe.call({
-		method:"production_api.production_api.doctype.goods_received_note.goods_received_note.get_grn_rework_items",
-		args: {
-			doc_name:frm.doc.name,
-			supplier:supplier,
-			supplier_address: supplier_address,
-			delivery_address: delivery_address,
-			rework_type:rework_type,
-			supplier_type: supplier_type,
-		},
-		callback: function(r){
-			if(r.message){
-				frappe.set_route("Form","Work Order", r.message);
-			}
-			else{
-				frappe.msgprint("There is no mistake items are received")
-			}
-		}
-	})
-}
 
 function calculate_receivables(frm, received_type){
 	let items = frm.calculate_receivables.get_work_order_items()
