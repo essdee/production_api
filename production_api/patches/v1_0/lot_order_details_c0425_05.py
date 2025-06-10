0bde021e6255a40f71d134ffa5a327b7531c0b46
Sakthi Kumar Pugalenthy <sakthi123msd@gmail.com>
2025-06-10 11:32:17 +0530

    fix : delete lot order item

--- production_api/patches/v1_0/lot_order_details_c0425_05.py
+++ production_api/patches/v1_0/lot_order_details_c0425_05.py
@@ -1,6 +1,12 @@
 
 import frappe
 def execute():
+
+  frappe.db.sql(
+    """
+      DELETE FROM `tabLot Order Item` WHERE parent = 'C0425-05';
+    """
+  )
   frappe.db.sql(
     """
       DELETE FROM `tabLot Order Detail` WHERE parent = 'C0425-05';
