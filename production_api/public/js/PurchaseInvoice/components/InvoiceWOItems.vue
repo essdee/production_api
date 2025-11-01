61a816bf3420c8d0bc801ea117efb5599dee5bd7
MohammedAnassBE <mohammedanasman123@gmail.com>
2025-11-01 17:30:26 +0530

    fix: PI against WO

--- production_api/public/js/PurchaseInvoice/components/InvoiceWOItems.vue
+++ production_api/public/js/PurchaseInvoice/components/InvoiceWOItems.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
-        <div style="text-align: end;" v-if="docstatus == 0">
-            <button class="btn btn-success" @click="approve()">Approve</button>
+        <div v-if="approved_by" style="text-align:end;">
+            <h4>Approved By Merch Manager</h4>
+        </div>
+        <div v-if="senior_approved" style="text-align:end;">
+            <h4>Approved By Senior Merch</h4>
         </div>
         <div class="mb-4">
             <h4>Amount Details</h4>
@@ -62,9 +65,9 @@
                     <tbody class="dark-border">
                         <template v-for="(colour, idx) in Object.keys(item['colours'])">
                             <tr>
-                                <td :rowspan="5">{{ idx + 1 }}</td>
-                                <td :rowspan="5">{{ colour.split("@")[0] }}</td>
-                                <td :rowspan="5" v-if="item['is_set_item']">{{ item['colours'][colour]['part'] }}</td>
+                                <td :rowspan="6">{{ idx + 1 }}</td>
+                                <td :rowspan="6">{{ colour.split("@")[0] }}</td>
+                                <td :rowspan="6" v-if="item['is_set_item']">{{ item['colours'][colour]['part'] }}</td>
                                 <td>Total Delivered</td>
                                 <td v-for="size in item['sizes']">{{ item['colours'][colour]['data'][size]['total_delivered'] }}</td>
                                 <th>{{ item['total_qty'][colour]['total_delivered'] }}</th>
@@ -75,12 +78,19 @@
                                 <th>{{ item['total_qty'][colour]['total_received'] }}</th>
                             </tr>
                             <tr>
+                               <td>Difference</td>
+                               <td v-for="size in item['sizes']">
+                                    {{ item['colours'][colour]['data'][size]['total_delivered'] - item['colours'][colour]['data'][size]['total_received'] }}
+                                </td>
+                               <th>{{ item['total_qty'][colour]['total_delivered'] - item['total_qty'][colour]['total_received'] }}</th>
+                            </tr>
+                            <tr>
                                 <td>Total Billed</td>
                                 <td v-for="size in item['sizes']">{{ item['colours'][colour]['data'][size]['billed'] }}</td>
                                 <th>{{ item['total_qty'][colour]['total_billed'] }}</th>
                             </tr>
                             <tr>
-                                <td>Pending</td>
+                                <td>Pending For Bill</td>
                                 <td v-for="size in item['sizes']">{{ item['colours'][colour]['data'][size]['total_received'] - item['colours'][colour]['data'][size]['billed'] }}</td>
                                 <th>{{ item['total_qty'][colour]['total_received'] - item['total_qty'][colour]['total_billed'] }}</th>
                             </tr>
@@ -94,6 +104,9 @@
                 </table>
             </div>
         </div>
+        <div style="text-align: end;" v-if="docstatus == 0 && !approved_by">
+            <button class="btn btn-success" @click="approve()">Approve</button>
+        </div>
     </div>
 </template>
 <script setup>
@@ -104,6 +117,9 @@ let items = ref([])
 let cost_value = ref([])
 let total_amount = ref(0)
 let docstatus = cur_frm.doc.docstatus
+let user_role = ref(null)
+let approved_by = cur_frm.doc.approved_by
+let senior_approved = cur_frm.doc.senior_merch_approved_by
 
 onMounted(() => {
     const grouped = {}
@@ -126,6 +142,14 @@ onMounted(() => {
         total_amount.value += amt
     })
     cost_value.value = Object.values(grouped)
+    frappe.call({
+        method: "production_api.production_api.doctype.purchase_invoice.purchase_invoice.get_merch_roles",
+        callback: function(r){
+            if(r.message){
+                user_role.value = r.message
+            }
+        }
+    })
 })
 
 function load_data(data){
@@ -135,13 +159,43 @@ function load_data(data){
 function approve(){
     let d = new frappe.ui.Dialog({
         title: "Are you sure want to Approve this Invoice",
+        fields: [
+            {
+                "fieldname": "comments",
+                "fieldtype": "Small Text",
+                "label": "Comments",
+            }
+        ],
         primary_action_label: "Yes",
         secondary_action_label: "No",
-        primary_action(){
-            d.hide()
-            cur_frm.doc.approved_by = frappe.session.user
-            cur_frm.refresh_field("approved_by")
+        primary_action(values){
             cur_frm.dirty()
+            d.hide()
+            console.log(user_role)
+            if (user_role.value === 'merch_manager') {
+                if (!senior_approved) {
+                    console.log("YESSSSSSSSSS")
+                    cur_frm.doc.approved_by = frappe.session.user
+                    cur_frm.set_value("approved_by", frappe.session.user);
+                    cur_frm.set_value("senior_merch_approved_by", frappe.session.user);
+                } 
+                else {
+                    console.log("YESS")
+                    cur_frm.set_value("approved_by", frappe.session.user);
+                }
+            } 
+            else if (user_role.value === 'senior_merch') {
+                cur_frm.set_value("senior_merch_approved_by", frappe.session.user);
+            }
+            cur_frm.refresh_field("approved_by")
+            cur_frm.refresh_field("senior_merch_approved_by")
+            cur_frm.add_child("purchase_invoice_wo_approval_details", {
+                user: frappe.session.user,
+                approved_time: frappe.datetime.now_datetime(),
+                comments: values.comments
+            });
+
+            cur_frm.refresh_field("purchase_invoice_wo_approval_details");
             cur_frm.save()
         },
         secondary_action(){
