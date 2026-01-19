19360ce3e481a4d97ea6b362c7f570ff4179dce9
MohammedAnassBE <mohammedanasman123@gmail.com>
2026-01-19 17:32:47 +0530

    feat: Sync with ERP item variants

--- production_api/production_api/doctype/item_variant/item_variant.py
+++ production_api/production_api/doctype/item_variant/item_variant.py
@@ -5,6 +5,19 @@ import frappe
 from frappe.model.document import Document
 
 class ItemVariant(Document):
+	def validate(self):
+		dept_attr = frappe.get_value("Item", self.item, "dependent_attribute")
+		if not dept_attr:
+			self.sync_with_erp = 1
+		else:
+			stich_attr = frappe.db.get_single_value("IPD Settings", "default_stitching_attribute")
+			sync = 1
+			for attribute in self.attributes:
+				if attribute.attribute == stich_attr:
+					sync = 0
+					break
+			self.sync_with_erp = sync	
+
 	def set_item(self):
 		item = frappe.get_doc("Item", self.item)
 		self.set_onload('__parent_item', item.as_dict())
