b53f64c37bc13347e91a93e255792f56f7ba4ec6
MohammedAnassBE <mohammedanasman123@gmail.com>
2026-01-31 17:01:39 +0530

    fix: Product Colour Images

---
+++ production_api/essdee_production/doctype/task_management/task_management.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, anas and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Task Management", {
+	refresh(frm) {
+		// your code here
+	},
+});
