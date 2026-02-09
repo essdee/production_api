c85c4f96fb912e5c90b2ea875c9a340393614c99
MohammedAnassBE <mohammedanasman123@gmail.com>
2026-02-09 10:34:53 +0530

    fix: removed task management

--- production_api/essdee_production/doctype/task_management/task_management.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, anas and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on("Task Management", {
-	refresh(frm) {
-		// your code here
-	},
-});
