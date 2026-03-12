6c0fd204e61f630d3af7c4a8eca94450950a1398
MohammedAnassBE <mohammedanasman123@gmail.com>
2026-03-12 18:06:22 +0530

    feat: Cutting Order

--- production_api/production_api/doctype/cutting_order/cutting_order.js
+++ production_api/production_api/doctype/cutting_order/cutting_order.js
@@ -2,15 +2,344 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Cutting Order", {
+	setup(frm) {
+		frm.trigger("declarations");
+
+		const setAttributeQuery = (doc) => {
+			const attributes = doc.item_attributes.map(attr => attr.attribute);
+			return { filters: { name: ["in", attributes] } };
+		};
+
+		frm.set_query('packing_attribute', setAttributeQuery);
+		frm.set_query('set_item_attribute', setAttributeQuery);
+		frm.set_query('stiching_attribute', setAttributeQuery);
+
+		frm.set_query('attribute_value', 'packing_attribute_details', () => {
+			if (!frm.doc.packing_attribute) {
+				frappe.throw("Please select the packing attribute first");
+			}
+			return {
+				query: 'production_api.essdee_production.doctype.item_production_detail.item_production_detail.get_attribute_detail_values',
+				filters: {
+					'mapping': frm.set_packing_attr_map_value,
+				}
+			};
+		});
+
+		frm.set_query('stiching_attribute_value', 'stiching_item_details', () => {
+			return {
+				query: 'production_api.essdee_production.doctype.item_production_detail.item_production_detail.get_attribute_detail_values',
+				filters: {
+					'mapping': frm.stiching_attribute_mapping,
+				}
+			};
+		});
+
+		frm.set_query('stiching_major_attribute_value', () => {
+			return {
+				query: 'production_api.essdee_production.doctype.item_production_detail.item_production_detail.get_attribute_detail_values',
+				filters: {
+					'mapping': frm.stiching_attribute_mapping,
+				}
+			};
+		});
+
+		frm.set_query('set_item_attribute_value', 'stiching_item_details', () => {
+			return {
+				query: 'production_api.essdee_production.doctype.item_production_detail.item_production_detail.get_attribute_detail_values',
+				filters: {
+					'mapping': frm.set_item_attr_map_value,
+				}
+			};
+		});
+
+		frm.set_query('major_attribute_value', () => {
+			if (!frm.doc.set_item_attribute) {
+				frappe.throw("Please set the Set Attribute Item");
+			}
+			return {
+				query: 'production_api.essdee_production.doctype.item_production_detail.item_production_detail.get_attribute_detail_values',
+				filters: {
+					'mapping': frm.set_item_attr_map_value,
+				}
+			};
+		});
+	},
+
+	declarations(frm) {
+		frm.set_packing_attr_map_value = null;
+		frm.set_item_attr_map_value = null;
+		frm.stiching_attribute_mapping = null;
+
+		for (let i = 0; i < frm.doc.item_attributes.length; i++) {
+			if (frm.doc.item_attributes[i].attribute == frm.doc.packing_attribute) {
+				frm.set_packing_attr_map_value = frm.doc.item_attributes[i].mapping;
+			}
+			if (frm.doc.item_attributes[i].attribute == frm.doc.set_item_attribute) {
+				frm.set_item_attr_map_value = frm.doc.item_attributes[i].mapping;
+			}
+			if (frm.doc.item_attributes[i].attribute == frm.doc.stiching_attribute) {
+				frm.stiching_attribute_mapping = frm.doc.item_attributes[i].mapping;
+			}
+		}
+	},
+
 	refresh(frm) {
-		if (frm.doc.__islocal) {
-			$(frm.fields_dict['item_attribute_list_values_html'].wrapper).html("");
-		} else if (frm.doc.__onload && frm.doc.__onload["attr_list"]) {
-			$(frm.fields_dict['item_attribute_list_values_html'].wrapper).html("");
+		frm.trigger('declarations');
+		frm.trigger('onload_post_render');
+
+		// Item Attribute List widget
+		$(frm.fields_dict['item_attribute_list_values_html'].wrapper).html("");
+		if (!frm.doc.__islocal && frm.doc.__onload && frm.doc.__onload["attr_list"]) {
 			new frappe.production.ui.ItemAttributeList({
 				wrapper: frm.fields_dict["item_attribute_list_values_html"].wrapper,
 				attr_values: frm.doc.__onload["attr_list"]
 			});
 		}
+
+		// Set Item combination
+		$(frm.fields_dict['set_items_html'].wrapper).html("");
+		if (!frm.doc.__islocal) {
+			if (!frm.doc.is_set_item) {
+				hide_field('set_items_html');
+			} else {
+				unhide_field('set_items_html');
+				frm.trigger('make_set_combination');
+			}
+		}
+
+		// Stitching combination
+		$(frm.fields_dict['stiching_items_html'].wrapper).html("");
+
+		frm.trigger('make_hide_and_unhide_tabs');
+	},
+
+	async make_set_combination(frm) {
+		$(frm.fields_dict['set_items_html'].wrapper).html("");
+		frm.set_item = new frappe.production.ui.CombinationItemDetail(frm.fields_dict['set_items_html'].wrapper);
+		if (frm.doc.__onload && frm.doc.__onload.set_item_detail) {
+			frm.doc['set_item_detail'] = JSON.stringify(frm.doc.__onload.set_item_detail);
+			await frm.set_item.load_data(frm.doc.__onload.set_item_detail);
+			frm.set_item.set_attributes();
+		}
+	},
+
+	async make_stiching_combination(frm) {
+		$(frm.fields_dict['stiching_items_html'].wrapper).html("");
+		frm.stiching_item = new frappe.production.ui.CombinationItemDetail(frm.fields_dict['stiching_items_html'].wrapper);
+		if (frm.doc.__onload && frm.doc.__onload.stiching_item_detail) {
+			frm.doc['stiching_item_detail'] = JSON.stringify(frm.doc.__onload.stiching_item_detail);
+			await frm.stiching_item.load_data(frm.doc.__onload.stiching_item_detail);
+			frm.stiching_item.set_attributes();
+		}
+	},
+
+	make_hide_and_unhide_tabs(frm) {
+		frm.trigger('make_stiching_combination');
+
+		if (!frm.doc.packing_attribute) {
+			frm.$wrapper.find("[data-fieldname='set_item_tab']").hide();
+		} else {
+			frm.$wrapper.find("[data-fieldname='set_item_tab']").show();
+		}
+	},
+
+	onload_post_render(frm) {
+		if (frm.fields_dict.stiching_item_details) {
+			showOrHideColumns(frm, ['set_item_attribute_value', 'is_default'], 'stiching_item_details', frm.doc.is_set_item ? 0 : 1);
+			updateChildTableReqd(frm, ['set_item_attribute_value', 'is_default'], 'stiching_item_details', frm.doc.is_set_item ? 1 : 0);
+		}
+	},
+
+	// Button handlers
+	get_packing_attribute_values(frm) {
+		frappe.call({
+			method: 'production_api.essdee_production.doctype.item_production_detail.item_production_detail.get_mapping_attribute_values',
+			args: {
+				attribute_mapping_value: frm.set_packing_attr_map_value,
+				attribute_no: frm.doc.packing_attribute_no,
+			},
+			callback: function(r) {
+				if (r.message) {
+					frm.set_value('packing_attribute_details', r.message);
+					frm.refresh_field('packing_attribute_details');
+				}
+			}
+		});
+	},
+
+	get_stiching_attribute_values(frm) {
+		frappe.call({
+			method: 'production_api.essdee_production.doctype.item_production_detail.item_production_detail.get_mapping_attribute_values',
+			args: {
+				attribute_mapping_value: frm.stiching_attribute_mapping,
+				attribute_no: null,
+			},
+			callback: function(r) {
+				if (r.message) {
+					frm.set_value('stiching_item_details', r.message);
+					frm.refresh_field('stiching_item_details');
+				}
+			}
+		});
+	},
+
+	get_set_item_combination(frm) {
+		if (!frm.doc.major_attribute_value) {
+			frappe.msgprint("Set the major attribute value");
+			return;
+		}
+		frappe.call({
+			method: 'production_api.essdee_production.doctype.item_production_detail.item_production_detail.get_new_combination',
+			args: {
+				attribute_mapping_value: frm.set_item_attr_map_value,
+				packing_attribute_details: frm.doc.packing_attribute_details,
+				major_attribute_value: frm.doc.major_attribute_value,
+			},
+			callback: async function(r) {
+				await frm.set_item.load_data(r.message);
+				frm.set_item.set_attributes();
+			}
+		});
+	},
+
+	get_stiching_item_combination(frm) {
+		if (!frm.doc.stiching_attribute) {
+			return;
+		}
+		if (!frm.doc.stiching_major_attribute_value) {
+			frappe.msgprint("Set the stiching major attribute value");
+			return;
+		}
+		if (frm.doc.stiching_item_details.length == 0) {
+			frappe.msgprint("Set the Stiching Item Detail");
+			return;
+		}
+		frappe.call({
+			method: 'production_api.production_api.doctype.cutting_order.cutting_order.get_co_new_combination',
+			args: {
+				attribute_mapping_value: frm.stiching_attribute_mapping,
+				packing_attribute_details: frm.doc.packing_attribute_details,
+				major_attribute_value: frm.doc.stiching_major_attribute_value,
+				is_same_packing_attribute: frm.doc.is_same_packing_attribute,
+				doc_name: frm.doc.name,
+			},
+			callback: async function(r) {
+				await frm.stiching_item.load_data(r.message);
+				frm.stiching_item.set_attributes();
+				frm.dirty();
+			}
+		});
+	},
+
+	// Field change handlers
+	is_set_item(frm) {
+		showOrHideColumns(frm, ['set_item_attribute_value', 'is_default'], 'stiching_item_details', frm.doc.is_set_item ? 0 : 1);
+		updateChildTableReqd(frm, ['set_item_attribute_value', 'is_default'], 'stiching_item_details', frm.doc.is_set_item ? 1 : 0);
+	},
+
+	set_item_attribute(frm) {
+		frm.set_item = new frappe.production.ui.CombinationItemDetail(frm.fields_dict['set_items_html'].wrapper);
+		if (frm.doc.major_attribute_value) {
+			frm.trigger('get_set_item_combination');
+		}
+		if (frm.doc.is_set_item && frm.doc.set_item_attribute) {
+			frm.trigger('declarations');
+			unhide_field(['set_items_html']);
+		}
+	},
+
+	packing_attribute(frm) {
+		if (frm.doc.packing_attribute) {
+			frm.trigger('declarations');
+		}
+		frm.trigger('make_hide_and_unhide_tabs');
+	},
+
+	major_attribute_value(frm) {
+		frm.trigger('get_set_item_combination');
+	},
+
+	stiching_attribute(frm) {
+		if (frm.doc.stiching_attribute) {
+			frm.trigger('declarations');
+		}
+	},
+
+	// Serialize Vue data before save
+	validate: async function(frm) {
+		if (!frm.doc.__islocal) {
+			if (frm.set_item && frm.doc.is_set_item) {
+				let item_details = frm.set_item.get_data();
+				frm.doc['set_item_detail'] = JSON.stringify(item_details);
+			}
+
+			if (frm.stiching_item) {
+				let item_details = frm.stiching_item.get_data();
+				if (item_details['values'].length > 0) {
+					frm.doc['stiching_item_detail'] = JSON.stringify(item_details);
+				}
+			}
+		}
 	},
 });
+
+
+function showOrHideColumns(frm, fields, table, hidden) {
+	if (frappe.ui.form.editable_row) {
+		frappe.ui.form.editable_row.toggle_editable_row(false);
+	}
+	let grid = frm.get_field(table).grid;
+	for (let field of fields) {
+		grid.fields_map[field].hidden = hidden;
+	}
+	grid.visible_columns = undefined;
+	grid.setup_visible_columns();
+
+	grid.header_row.wrapper.remove();
+	delete grid.header_row;
+	grid.make_head();
+
+	for (let row of grid.grid_rows) {
+		if (row.open_form_button) {
+			row.open_form_button.parent().remove();
+			delete row.open_form_button;
+		}
+		for (let field in row.columns) {
+			if (row.columns[field] !== undefined) {
+				row.columns[field].remove();
+			}
+		}
+		for (let fieldname of fields) {
+			let df = row.docfields.find(field => field.fieldname === fieldname);
+			df && (df.hidden = hidden);
+		}
+		delete row.columns;
+		row.columns = [];
+		row.render_row();
+	}
+	frappe.ui.form.editable_row && frappe.ui.form.editable_row.toggle_editable_row(false);
+}
+
+function updateChildTableReqd(frm, fields, table, reqd) {
+	let grid = frm.get_field(table).grid;
+	for (let row of grid.grid_rows) {
+		if (row.open_form_button) {
+			row.open_form_button.parent().remove();
+			delete row.open_form_button;
+		}
+		for (let field in row.columns) {
+			if (row.columns[field] !== undefined) {
+				row.columns[field].remove();
+			}
+		}
+		for (let fieldname of fields) {
+			let df = row.docfields.find(field => field.fieldname === fieldname);
+			df && (df.reqd = reqd);
+		}
+		delete row.columns;
+		row.columns = [];
+		row.render_row();
+	}
+	frappe.ui.form.editable_row && frappe.ui.form.editable_row.toggle_editable_row(false);
+}
--- production_api/production_api/doctype/cutting_order/cutting_order.py
+++ production_api/production_api/doctype/cutting_order/cutting_order.py
@@ -2,7 +2,12 @@
 # For license information, please see license.txt
 
 import frappe
+import json
 from frappe.model.document import Document
+from production_api.essdee_production.doctype.item_production_detail.item_production_detail import (
+	fetch_combination_items, save_item_details,
+)
+from production_api.utils import update_if_string_instance
 
 
 class CuttingOrder(Document):
@@ -11,6 +16,14 @@ class CuttingOrder(Document):
 
 	def onload(self):
 		self.load_attribute_list()
+
+		set_items = fetch_combination_items(self.get('set_item_combination_details'))
+		if len(set_items['values']) > 0:
+			self.set_onload('set_item_detail', set_items)
+
+		stich_items = fetch_combination_items(self.get('stiching_item_combination_details'))
+		if len(stich_items['values']) > 0:
+			self.set_onload('stiching_item_detail', stich_items)
 
 	def load_attribute_list(self):
 		attribute_list = []
@@ -35,3 +48,180 @@ class CuttingOrder(Document):
 						'doctype': 'Item Item Attribute Mapping'
 					})
 		self.set_onload('attr_list', attribute_list)
+
+	def before_validate(self):
+		if self.get('set_item_detail') and self.is_set_item:
+			set_details = save_item_details(self.set_item_detail)
+			self.set('set_item_combination_details', set_details)
+
+		if self.get('stiching_item_detail'):
+			stiching_detail = save_item_details(self.stiching_item_detail, ipd_doc=self)
+			self.set('stiching_item_combination_details', stiching_detail)
+
+		if not self.is_set_item:
+			self.set('set_item_combination_details', [])
+			self.set('major_attribute_value', None)
+			self.set('set_item_attribute', None)
+
+		if self.is_set_item and not self.is_new():
+			mapping = None
+			for item in self.item_attributes:
+				if item.attribute == self.set_item_attribute:
+					mapping = item.mapping
+					break
+			if mapping:
+				map_doc = frappe.get_cached_doc("Item Item Attribute Mapping", mapping)
+				map_values = [v.attribute_value for v in map_doc.values]
+
+				check_dict = {}
+				for attr in self.stiching_item_details:
+					if attr.is_default:
+						if check_dict.get(attr.set_item_attribute_value):
+							frappe.throw(f"Select only one Is Default for {attr.set_item_attribute_value}")
+						else:
+							check_dict[attr.set_item_attribute_value] = 1
+
+				if len(check_dict) < len(map_values):
+					frappe.throw("Select Is default for all Set Item Attributes")
+
+	def validate(self):
+		# Delete orphaned mappings for removed attribute rows
+		if not self.is_new():
+			previous = self.get_doc_before_save()
+			if previous:
+				current_mappings = {a.mapping for a in self.get('item_attributes') if a.mapping}
+				for attr in previous.get('item_attributes'):
+					if attr.mapping and attr.mapping not in current_mappings:
+						frappe.delete_doc("Item Item Attribute Mapping", attr.mapping, ignore_permissions=True)
+
+		# Auto-create mappings for new attribute rows
+		for attribute in self.get('item_attributes'):
+			if not attribute.mapping:
+				doc = frappe.new_doc("Item Item Attribute Mapping")
+				doc.attribute_name = attribute.attribute
+				doc.save()
+				attribute.mapping = doc.name
+
+		if not self.is_new():
+			if self.packing_attribute:
+				self.packing_tab_validations()
+
+			if self.stiching_attribute and len(self.stiching_item_details) > 0:
+				self.stiching_tab_validations()
+
+	def packing_tab_validations(self):
+		if self.packing_combo == 0:
+			frappe.throw("The packing combo should not be zero")
+
+		if self.packing_attribute_no == 0:
+			frappe.throw("The packing attribute no should not be zero")
+
+		mapping = None
+		for item in self.item_attributes:
+			if item.attribute == self.packing_attribute:
+				mapping = item.mapping
+				break
+
+		if mapping:
+			map_doc = frappe.get_cached_doc("Item Item Attribute Mapping", mapping)
+			if len(map_doc.values) < self.packing_attribute_no:
+				frappe.throw(f"The Packing attribute no is {self.packing_attribute_no} But there is only {len(map_doc.values)} attributes are available")
+
+		if len(self.packing_attribute_details) != self.packing_attribute_no:
+			frappe.throw(f"Only {self.packing_attribute_no} {self.packing_attribute}'s are required")
+
+		sum_qty = 0.0
+		attr = set()
+		for row in self.packing_attribute_details:
+			if not row.quantity:
+				frappe.throw("Enter value in Packing Attribute Details, Zero is not considered as a valid quantity")
+			sum_qty += row.quantity
+			attr.add(row.attribute_value)
+
+		if sum_qty < self.packing_combo or sum_qty > self.packing_combo:
+			frappe.throw(f"In Packing Attribute Details, the sum of quantity should be {self.packing_combo}")
+
+		if len(attr) != len(self.packing_attribute_details):
+			frappe.throw("Duplicate Attribute values are occured in Packing Attribute Details")
+
+	def stiching_tab_validations(self):
+		if len(self.stiching_item_details) == 0:
+			frappe.throw("Enter stiching attribute details")
+
+		attr = set()
+		for row in self.stiching_item_details:
+			if not row.quantity:
+				frappe.throw("Enter value in Stiching Item Details, Zero is not considered as a valid quantity")
+			attr.add(row.stiching_attribute_value)
+
+		if len(attr) != len(self.stiching_item_details):
+			frappe.throw("Duplicate Attribute values are occured in Stiching Item Details")
+
+
+@frappe.whitelist()
+def update_attribute_mapping_values(mapping, values):
+	if isinstance(values, str):
+		values = json.loads(values)
+	doc = frappe.get_doc("Item Item Attribute Mapping", mapping)
+	doc.set("values", [])
+	for v in values:
+		doc.append("values", {"attribute_value": v})
+	doc.save()
+
+
+@frappe.whitelist()
+def get_co_new_combination(attribute_mapping_value, packing_attribute_details, major_attribute_value, is_same_packing_attribute=False, doc_name=None):
+	packing_attribute_details = update_if_string_instance(packing_attribute_details)
+	if isinstance(packing_attribute_details, list):
+		pass
+	elif isinstance(packing_attribute_details, dict):
+		packing_attribute_details = list(packing_attribute_details.values()) if packing_attribute_details else []
+
+	doc = frappe.get_cached_doc('Item Item Attribute Mapping', attribute_mapping_value)
+	attributes = [item.attribute_value for item in doc.values]
+
+	stiching_item_details = {}
+	set_item_details = {}
+	is_default_list = []
+	co_doc = None
+
+	if doc_name:
+		co_doc = frappe.get_doc("Cutting Order", doc_name)
+		if co_doc.is_set_item:
+			for item in co_doc.stiching_item_details:
+				stiching_item_details[item.stiching_attribute_value] = item.set_item_attribute_value
+				if item.is_default:
+					is_default_list.append(item.stiching_attribute_value)
+
+			for item in co_doc.set_item_combination_details:
+				set_item_details.setdefault(item.major_attribute_value, {})
+				set_item_details[item.major_attribute_value][item.set_item_attribute_value] = item.attribute_value
+
+	if isinstance(is_same_packing_attribute, str):
+		is_same_packing_attribute = is_same_packing_attribute in ('true', 'True', '1')
+
+	item_detail = []
+	for item in packing_attribute_details:
+		item_list = {}
+		item_list['major_attribute'] = item['attribute_value']
+		item_list['val'] = {}
+		for i in attributes:
+			if i == major_attribute_value:
+				item_list['val'][i] = item['attribute_value']
+			elif is_same_packing_attribute:
+				if doc_name and co_doc and co_doc.is_set_item:
+					part = stiching_item_details[i]
+					item_list['val'][i] = set_item_details[item['attribute_value']][part]
+				else:
+					item_list['val'][i] = item['attribute_value']
+			elif doc_name and co_doc and co_doc.is_set_item and i in is_default_list:
+				part = stiching_item_details[i]
+				item_list['val'][i] = set_item_details[item['attribute_value']][part]
+			else:
+				item_list['val'][i] = None
+		item_detail.append(item_list)
+
+	item_details = {}
+	item_details['attributes'] = attributes
+	item_details['values'] = item_detail
+	return item_details
